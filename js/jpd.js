df08b958f232459bd75240d0cd66c86cc36b6f75
Luckyyy <jaaniii@12345678@gmail,com>
2022-08-19 19:41:05 +0530

    Update jpd.js

--- js/jpd.js
+++ js/jpd.js
@@ -1,10 +1,7 @@
   alert("DANGER!!!!");
   let name1 = prompt("Enter passcode: ","");
   if(name1 == "panda"){
-    alert(">>>>>>>>>>>");
-    window.open("#","_parent");
   }
   else{
     window.close();
-    
   }
