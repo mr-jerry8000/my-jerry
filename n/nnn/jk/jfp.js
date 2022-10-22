c5183acd75d8867a2d81ae7b15b0c9e59b2f4d5e
Luckyyy <jaaniii@12345678@gmail,com>
2022-10-22 13:10:03 +0530

    new update

---
+++ n/nnn/jk/jfp.js
@@ -0,0 +1,21 @@
+
+    function validate() {
+      var password = document.getElementById("password").value;
+      if (password == 87699) {
+        // alert("login Successfully!!");
+        window.open("coming.html", "_self");
+        // window.location.href = "http://www.google.com";
+        return false;
+      }
+       else {
+        // alert("Login failed");
+        document.getElementById("message").innerHTML = "Enter valid username and password!!!";  
+      }
+    }
+    function done(){
+      document.getElementById("message2").innerHTML = "Enter successfully!!!";  
+    }
+    
+    
+    
+ 
